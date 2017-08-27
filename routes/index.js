f0d19540e39015b5135c16780ebab27326476f8a
bdunams <dabadestboy1@gmail.com>
2017-08-27 19:30:46 -0400

    Added update password functionality

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,7 @@ router.get('/', function(req, res, next) {
   
 });
 
+
 // GET Account Settings Page
 router.get('/user/account', function(req, res, next) {
   
@@ -33,18 +34,72 @@ router.get('/user/account', function(req, res, next) {
     
 });
 
+
+
 // UPDATE User Password
 router.post('/user/update-password', function(req, res, next) {
   
-  if(req.user){
-    
-    let password = req.body.password;
-    let confirmPassword = req.body.confirmPassword;
-    
-    res.render('account');
+  // validate password update
+  let oldPassword = req.body.oldPassword;
+  let password = req.body.password;
+  let confirmPassword = req.body.confirmPassword;
+
+  req.checkBody('oldPassword', 'Old password is required').notEmpty();
+  req.checkBody('password', 'New Password is required').notEmpty();
+  req.checkBody('confirmPassword', 'Passwords do not match').equals(req.body.password);
+
+  // Get Validation Results
+  let errors = req.validationErrors();
+  
+  // IF errors, render errors to user
+  if(errors){
+
+    return res.render('account',{
+        error_msg: errors
+      });
+
   }
+  
+  // ELSE continue updating user password
   else{
-    res.redirect('/');
+  
+  
+    // IF a user is logged in and enters correct current password
+    if(db.User.checkPassword(req.body.oldPassword, req.user.password)){
+     
+      // Hash Password with Bcryptjs
+      let securePassword = db.User.hashPassword(password);
+      
+      // Update new password in database
+      db.User.update({
+        password: securePassword
+      },{
+        where: {
+          id: req.user.id
+        }
+      })
+      .then((user) =>{
+        
+        res.render('account',{
+          success_msg: 'Successfully updated password!'
+        });
+      })
+      .catch((err) => {
+        
+        res.render('account',{
+          error_msg: 'Password update was not successful'
+        });
+      })
+      
+    }
+    // ELSE old password didnt match the current password
+    else{
+
+      return res.render('account',{
+            error_msg: {msg:'Password did not match'}
+          });
+    }
+    
   }
     
 });
